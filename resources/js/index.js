b6ec200a6a539901902baedcc7c8fc72b7944d01
DanielMM161 <daniel-moreno-marquez@hotmail.es>
2020-05-07 19:22:49 +0200

    Commit inicial del proyecto

---
+++ resources/js/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import reducers from './reducers';
+// Redux
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+)
+
+if (document.getElementById('app')) {
+    ReactDOM.render(
+        <Provider store={ store }>
+            <App />    
+        </Provider>, 
+        document.getElementById('app')
+    );
+}
